5e8115c428b2e343092e3f6498e548b12ccc742e
hobnob11 <williamhobson62@aol.com>
2012-10-17 14:57:20 +0100

    Switched to FoxCavesAPI
    Now actualy works O.o

--- FileUploader/FileUploader/Module1.vb
+++ FileUploader/FileUploader/Module1.vb
@@ -6,9 +6,13 @@ Imports FoxScreen
 Module Module1
     Public File As String
     Public AppPath As String
+    Public Password As String
     Sub Main()
 
         Console.WriteLine("Ok, Whist this is still in like alpha version -0.1, the file being uploaded is going to be upload.txt in the same place as the exe")
+        Console.WriteLine("Password:  ")
+        Password = Console.ReadLine()
+
         System.Threading.Thread.Sleep(2000)
 
         AppPath = System.AppDomain.CurrentDomain.BaseDirectory
@@ -16,7 +20,9 @@ Module Module1
 
             File = AppPath & "upload.txt"
 
-            Upload(File, "UploadTest.txt")
+            If (Upload(File, "UploadTest.txt") = True) Then
+                Stop
+            End If
 
         Else
 
@@ -33,15 +39,23 @@ Module Module1
     Function Upload(ByVal FileDir As String, ByVal FileName As String) As Boolean
 
         Dim MemoryStream As MemoryStream = New MemoryStream(My.Computer.FileSystem.ReadAllBytes(FileDir))
-        Dim FoxDLL As New FoxScreen.UploadOrganizer
+        Dim FoxDLL As New FoxCavesAPI.Uploader()
 
 
 
-        FoxDLL.AddUpload(FileName, MemoryStream)
+        FoxDLL.SetCredentials("hobnob11", Password)
 
+        If (FoxDLL.QueueSync(FileName, MemoryStream) = vbNullString) Then
 
+            Console.WriteLine("SOMTHING WENT WRONG IN DORIS API, PROBBABLY FROM SOMTHING I ENTERED, YOU MAY NOW PANIC.")
+            System.Threading.Thread.Sleep(2000)
+            Upload = False
 
-        Upload = True
+        Else
+            Console.WriteLine("UploadComplete")
+            Upload = True
+
+        End If
 
     End Function
 
