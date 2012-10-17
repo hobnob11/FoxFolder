88a750470d82994c1049e1f4c292a021c13b668f
hobnob11 <williamhobson62@aol.com>
2012-10-17 22:57:49 +0100

    0.0.2
    New features include:
    +Full login, now doesnt assume your me
    +can upload all the files in the same folder! (not files in sub folders
    yet)
    +uses the new FoxCavesAPI which includes new features that i "will" use
    -now doesnt assume that the only thing to be uploaded is upload.txt

--- FileUploader/FileUploader/Module1.vb
+++ FileUploader/FileUploader/Module1.vb
@@ -1,41 +1,61 @@
 ﻿Imports System
 Imports System.IO
 Imports System.Text
-Imports FoxScreen
+Imports FoxCavesAPI
 
 Module Module1
     Public File As String
     Public AppPath As String
     Public Password As String
+    Public Username As String
     Sub Main()
 
-        Console.WriteLine("Ok, Whist this is still in like alpha version -0.1, the file being uploaded is going to be upload.txt in the same place as the exe")
+        Console.WriteLine("FoxFolder0.0.2, Now with actual multiple file upload, Most likey cause of failure is API update or wrong login.")
+        Console.WriteLine("To use, place in folder, run, login, done :)")
+        Console.WriteLine("Created using FoxCaveAPI, Made by Dordian: foxcav.es")
+        Console.WriteLine("Side note, the program currantly exits by crashing, the alternative worse, trust me.")
+        Console.WriteLine("Username:  ")
+        Username = Console.ReadLine()
         Console.WriteLine("Password:  ")
         Password = Console.ReadLine()
 
         System.Threading.Thread.Sleep(2000)
 
         AppPath = System.AppDomain.CurrentDomain.BaseDirectory
-        If (My.Computer.FileSystem.FileExists(AppPath & "upload.txt")) Then
 
-            File = AppPath & "upload.txt"
+        Dim FileName As String
 
-            If (Upload(File, "UploadTest.txt") = True) Then
-                Stop
-            End If
 
-        Else
 
-            MsgBox("File upload.txt does not exist, please create this file then try again")
+            Console.WriteLine(AppPath)
+            For Each foundFile As String In My.Computer.FileSystem.GetFiles(AppPath)
+                FileName = My.Computer.FileSystem.GetName(foundFile)
 
-        End If
+                If (Upload(foundFile, FileName) = True) Then
+
+                    Console.WriteLine("Uploaded " & FileName)
+
+                Else
+
+                    Console.WriteLine("Could not upload " & FileName)
+
+                End If
+        Next
+
+
+        Console.WriteLine("Press Enter to exit")
+        Console.ReadKey()
+        Stop
+
+
+    End Sub
+
+    Private Sub Login()
 
 
 
 
     End Sub
-
-
     Function Upload(ByVal FileDir As String, ByVal FileName As String) As Boolean
 
         Dim MemoryStream As MemoryStream = New MemoryStream(My.Computer.FileSystem.ReadAllBytes(FileDir))
@@ -43,16 +63,11 @@ Module Module1
 
 
 
-        FoxDLL.SetCredentials("hobnob11", Password)
+        FoxDLL.SetCredentials(Username, Password)
 
-        If (FoxDLL.QueueSync(FileName, MemoryStream) = vbNullString) Then
-
-            Console.WriteLine("SOMTHING WENT WRONG IN DORIS API, PROBBABLY FROM SOMTHING I ENTERED, YOU MAY NOW PANIC.")
-            System.Threading.Thread.Sleep(2000)
+        If (FoxDLL.QueueSync(FileName, MemoryStream) = "") Then
             Upload = False
-
         Else
-            Console.WriteLine("UploadComplete")
             Upload = True
 
         End If
